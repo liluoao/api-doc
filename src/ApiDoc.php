a1d4ee2125bcbe769b05e96af013357c8896a852
liluoao <liluoao@qq.com>
2018-02-11 10:53:56 +0800

    Change code format

--- src/ApiDoc.php
+++ src/ApiDoc.php
@@ -3,8 +3,7 @@
 /**
  * Class ApiDoc
  */
-class ApiDoc
-{
+class ApiDoc {
 
     /**
      * 模板文件扩展名
@@ -78,13 +77,18 @@ class ApiDoc
      * @param $documentPath
      * @param null $savePath
      */
-    public function __construct ($documentPath, $savePath = null)
-    {
+    public function __construct($documentPath, $savePath = null) {
+
         $this->setDocumentPath($documentPath);
+
         if (is_null($savePath)) {
+
             $this->setSavePath(getcwd() . DIRECTORY_SEPARATOR);
+
         } else {
+
             $this->setSavePath($savePath);
+
         }
     }
 
@@ -97,8 +101,7 @@ class ApiDoc
      * @param int $methodFrequency 方法名中出现大写字母次数时转换
      * @return void
      */
-    public function setCamel2SnakeConfig ($openController = true, $openMethod = true, $controllerFrequency = 1, $methodFrequency = 2): void
-    {
+    public function setCamel2SnakeConfig($openController = true, $openMethod = true, $controllerFrequency = 1, $methodFrequency = 2): void {
         $this->openControllerChange = $openController;
         $this->openMethodChange = $openMethod;
         $this->controllerFrequency = $controllerFrequency;
@@ -109,16 +112,18 @@ class ApiDoc
      * camelCase to snake_case
      *
      * @param string $str 字符串
-     * @param integer $times 出现几次大写字母才转换,默认1次
+     * @param integer $upperTimes 出现几次大写字母才转换,默认1次
      * @return string
      */
-    private function camelCase2SnakeCase ($str, $times = 1)
-    {
-        if (preg_match_all('/[A-Z]/', $str) >= $times) {
+    private function camelCase2SnakeCase($str, $upperTimes = 1) {
+
+        if (preg_match_all('/[A-Z]/', $str) >= $upperTimes) {
+
             $str = preg_replace_callback('/([A-Z]{1})/', function ($matches) {
                 return '_' . strtolower($matches[0]);
             }, $str);
-            if ($str[0] == '_') {
+
+            if ('_' === $str[0]) {
                 $str = substr_replace($str, '', 0, 1);
             }
 
@@ -134,12 +139,14 @@ class ApiDoc
      * @param bool $fetch 是否直接输出
      * @return bool|mixed|string
      */
-    public function init ($fetch = false)
-    {
+    public function init($fetch = false) {
+
         $fileList = [];
+
         $this->getAllFileInPath($this->getDocumentPath(), $fileList);
-        $inputData = ''; // 主体部分表格
-        $rightList = []; // 侧边栏列表
+
+        $tableData = '';
+        $sidebar = []; // 侧边栏列表
 
         foreach ($fileList as $fileName) {
 
@@ -148,39 +155,43 @@ class ApiDoc
 
             foreach ($data as $oneDoc) {
                 $infoData = $this->parse($oneDoc, $fileName);
-                $rightList[basename($fileName)][] = [
+                $sidebar[basename($fileName)][] = [
                     'methodName' => $infoData['methodName'],
                     'requestUrl' => $infoData['requestUrl'],
                 ];
-                $inputData .= $this->generateTable($infoData);
+                $tableData .= $this->generateTable($infoData);
             }
         }
 
         $templateFile = dirname(__FILE__) . DIRECTORY_SEPARATOR . $this->getTemplateName() . self::TEMPLATE_FILE_EXTENSION;
-        $tempData = file_get_contents($templateFile);
-        $tempData = str_replace('{name}', $this->getName(), $tempData);
-        $tempData = str_replace('{main}', $inputData, $tempData);
-        $tempData = str_replace('{right}', $this->makeRight($rightList), $tempData);
-        $tempData = str_replace('{date}', date('Y-m-d H:i:s'), $tempData);
+        $templateData = file_get_contents($templateFile);
+        $result = str_replace('{name}', $this->getName(), $templateData);
+        $result = str_replace('{main}', $tableData, $result);
+        $result = str_replace('{right}', $this->generateSidebar($sidebar), $result);
+        $result = str_replace('{date}', date('Y-m-d H:i:s'), $result);
 
         if (!$fetch) {
-            file_put_contents($this->getSaveFilePathAndName(), $tempData);
+
+            file_put_contents($this->getSaveFilePathAndName(), $result);
             exit();
+
         } else {
-            return $tempData;
+
+            return $result;
         }
     }
 
     /**
-     * 获取文件夹下的所有文件
+     * 获取文件夹下的所有PHP(可设置)文件
      *
      * @param string $path 路径
      * @param array $fileList 结果保存的变量
      * @param bool $all 可选,true全部,false当前路径下,默认true.
      */
-    private function getAllFileInPath ($path, &$fileList = [], $all = true)
-    {
+    private function getAllFileInPath($path, &$fileList = [], $all = true) {
+
         if (!is_dir($path)) {
+
             $fileList = [];
 
             return;
@@ -189,7 +200,7 @@ class ApiDoc
 
         foreach ($data as $oneFile) {
 
-            if ($oneFile == '.' || $oneFile == '..') {
+            if ('.' === $oneFile || '..' === $oneFile) {
                 continue;
             }
 
@@ -217,8 +228,7 @@ class ApiDoc
      * @param string $filename 完整文件名
      * @return string
      */
-    protected function getFileExtension ($filename)
-    {
+    protected function getFileExtension($filename) {
         return strrchr($filename, '.');
     }
 
@@ -228,14 +238,15 @@ class ApiDoc
      * @param string $data 代码文件内容
      * @return array
      */
-    private function getAllNeedTransDoc ($data)
-    {
-        preg_match_all('/(\/\*\*.*?\*\sapi.*?\*\/\s*(public|private|protected)?\s*function\s+.*?\s*?\()/s', $data, $matches);
-        if (empty($matches[1])) {
-            return [];
-        } else {
-            return $matches[1];
-        }
+    private function getAllNeedTransDoc($data) {
+
+        preg_match_all(
+            '/(\/\*\*.*?\*\sapi.*?\*\/\s*(public|private|protected)?\s*function\s+.*?\s*?\()/s',
+            $data,
+            $matches
+        );
+
+        return $matches[1] ?? [];
     }
 
     /**
@@ -245,65 +256,93 @@ class ApiDoc
      * @param string $fileName 文件名
      * @return array
      */
-    private function parse ($data, $fileName)
-    {
+    private function parse($data, $fileName) {
         $fileName = basename($fileName);
         $return = [];
 
-        preg_match_all('/(public|private|protected)?\s*function\s+(.*?)\(/', $data, $matches);
-        $return['funcName'] = !empty($matches[2][0]) ? $matches[2][0] : '[null]';
+        preg_match_all(
+            '/(public|private|protected)?\s*function\s+(.*?)\(/',
+            $data,
+            $matches
+        );
+        $return['funcName'] = $matches[2][0] ?: '[null]';
 
-        preg_match_all('/\/\*\*\s+\*\s+(.*?)\s+\*\s+api\s+/s', $data, $matches);
-        $return['methodName'] = !empty($matches[1][0]) ? $matches[1][0] : '[null]';
+        preg_match_all(
+            '/\/\*\*\s+\*\s+(.*?)\s+\*\s+api\s+/s',
+            $data,
+            $matches
+        );
+        $return['methodName'] = $matches[1][0] ?: '[null]';
 
-        preg_match_all('/\s+\*\s+api\s+(.*?)\s+(.*?)\s+(\s+\*\s+@)?.*/', $data, $matches);
-        $return['requestName'] = !empty($matches[1][0]) ? $matches[1][0] : '[null]';
-        $return['requestUrl'] = !empty($matches[2][0]) ? $matches[2][0] : '[null]';
+        preg_match_all(
+            '/\s+\*\s+api\s+(.*?)\s+(.*?)\s+(\s+\*\s+@)?.*/',
+            $data,
+            $matches
+        );
+        $return['requestName'] = $matches[1][0] ?: '[null]';
+        $return['requestUrl'] = $matches[2][0] ?: '[null]';
 
         if ($this->openControllerChange) {
-            $return['requestUrl'] = str_replace('{controller}', $this->camelCase2SnakeCase($fileName, $this->controllerFrequency), $return['requestUrl']);
+            $return['requestUrl'] = str_replace(
+                '{controller}',
+                $this->camelCase2SnakeCase($fileName, $this->controllerFrequency),
+                $return['requestUrl']
+            );
         }
         if ($this->openMethodChange) {
-            $return['requestUrl'] = str_replace('{method}', $this->camelCase2SnakeCase($return['funcName'], $this->methodFrequency), $return['requestUrl']);
+            $return['requestUrl'] = str_replace(
+                '{method}',
+                $this->camelCase2SnakeCase($return['funcName'], $this->methodFrequency),
+                $return['requestUrl']
+            );
         }
 
-        preg_match_all('/\s+\*\s+@param\s+(.*?)\s+(.*?)\s+(.*?)\s/', $data, $matches);
-        if (empty($matches[1])) {
+        preg_match_all(
+            '/\s+\*\s+@param\s+(.*?)\s+(.*?)\s+(.*?)\s/',
+            $data,
+            $matches
+        );
 
-            $return['param'] = [];
+        $return['param'] = [];
 
-        } else {
+        if (!empty($matches[1])) {
 
             for ($i = 0; $i < count($matches[1]); $i++) {
+
                 $return['param'][] = [
-                    'type' => !empty($matches[1][$i]) ? $matches[1][$i] : '[null]',
-                    'var' => !empty($matches[2][$i]) ? $matches[2][$i] : '[null]',
-                    'about' => !empty($matches[3][$i]) ? $matches[3][$i] : '[null]'
+                    'type' => $matches[1][$i] ?: '[null]',
+                    'var' => $matches[2][$i] ?: '[null]',
+                    'description' => $matches[3][$i] ?: '[null]'
                 ];
             }
 
         }
 
-        preg_match_all('/\s+\*\s+@return\s+(.*?)\s+(.*?)\s+(.*?)\s/', $data, $matches);
-        if (empty($matches[1])) {
+        preg_match_all(
+            '/\s+\*\s+@return\s+(.*?)\s+(.*?)\s+(.*?)\s/',
+            $data,
+            $matches
+        );
 
-            $return['return'] = [];
+        $return['return'] = [];
 
-        } else {
+        if (!empty($matches[1])) {
+
             for ($i = 0; $i < count($matches[1]); $i++) {
-                $type = !empty($matches[1][$i]) ? $matches[1][$i] : '[null]';
-                $var = !empty($matches[2][$i]) ? $matches[2][$i] : '[null]';
-                $about = !empty($matches[3][$i]) ? $matches[3][$i] : '[null]';
-                if (strpos($about, '*/')) {
-                    $about = $var;
+                $type = $matches[1][$i] ?: '[null]';
+                $var = $matches[2][$i] ?: '[null]';
+                $description = $matches[3][$i] ?: '[null]';
+                if (false !== strpos($description, '*/')) {
+                    $description = $var;
                     $var = '';
                 }
                 $return['return'][] = [
                     'type' => $type,
                     'var' => $var,
-                    'about' => $about,
+                    'description' => $description,
                 ];
             }
+
         }
 
         return $return;
@@ -315,27 +354,35 @@ class ApiDoc
      * @param array $data 解析后的API信息
      * @return string
      */
-    private function generateTable ($data)
-    {
-        $return = $this->getHeader($data);
+    private function generateTable($data) {
+
+        $tableDataString = $this->getHeader($data);
+
         if (count($data['param']) > 0) {
-            $return .= $this->getTableHeader('请求');
-            foreach ($data['param'] as $param) {
-                $return .= $this->getTableContent($param);
+
+            $tableDataString .= $this->getTableHeader('请求');
+
+            foreach ($data['param'] as $tr) {
+
+                $tableDataString .= $this->getTableContent($tr);
             }
-            $return .= $this->getTableBottom();
+            $tableDataString .= $this->getTableBottom();
         }
+
         if (count($data['return']) > 0) {
-            $return .= $this->getTableHeader('返回');
-            foreach ($data['return'] as $param) {
-                $return .= $this->getTableContent($param);
+
+            $tableDataString .= $this->getTableHeader('返回');
+
+            foreach ($data['return'] as $tr) {
+
+                $tableDataString .= $this->getTableContent($tr);
             }
-            $return .= $this->getTableBottom();
+            $tableDataString .= $this->getTableBottom();
         }
 
-        $return .= $this->getBottom();
+        $tableDataString .= $this->getBottom();
 
-        return $return;
+        return $tableDataString;
     }
 
     /**
@@ -344,8 +391,7 @@ class ApiDoc
      * @param array $data
      * @return string
      */
-    protected function getHeader ($data)
-    {
+    protected function getHeader($data) {
         return '<div id="' . base64_encode($data['requestUrl']) . '" class="api-main">
         <div class="title">' . $data['methodName'] . '</div>
         <div class="body">
@@ -370,45 +416,31 @@ class ApiDoc
      * @param string $prefix
      * @return string
      */
-    protected function getTableHeader ($prefix)
-    {
-        return '                    <div class="body">
-            <table class="layui-table">
-                <thead>
-                    <tr>
-                        <th>
-                            ' . $prefix . '名称
-                        </th>
-                        <th>
-                            ' . $prefix . '类型
-                        </th>
-                        <th>
-                            ' . $prefix . '说明
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>';
+    protected function getTableHeader($prefix) {
+        return '<div class="body">
+                <table class="layui-table">
+                    <thead>
+                        <tr>
+                            <th>' . $prefix . '名称</th>
+                            <th>' . $prefix . '类型</th>
+                            <th>' . $prefix . '说明</th>
+                        </tr>
+                    </thead>
+                    <tbody>';
     }
 
     /**
      * 获取表格内容
      *
-     * @param array $param
+     * @param array $tr
      * @return string
      */
-    protected function getTableContent ($param)
-    {
-        return '<tr>
-                    <td>
-                        ' . $param['var'] . '
-                    </td>
-                    <td>
-                    ' . $param['type'] . '
-                    </td>
-                    <td>
-                    ' . $param['about'] . '
-                    </td>
-                </tr>';
+    protected function getTableContent($tr) {
+        return "<tr>
+                    <td>{$tr['var']}</td>
+                    <td>{$tr['type']}</td>
+                    <td>{$tr['description']}</td>
+                </tr>";
     }
 
     /**
@@ -416,11 +448,8 @@ class ApiDoc
      *
      * @return string
      */
-    protected function getTableBottom ()
-    {
-        return '</tbody>
-            </table>
-        </div>';
+    protected function getTableBottom() {
+        return '</tbody></table></div>';
     }
 
     /**
@@ -428,70 +457,68 @@ class ApiDoc
      *
      * @return string
      */
-    protected function getBottom ()
-    {
-        return ' <hr>
-        </div>';
+    protected function getBottom() {
+        return '<hr></div>';
     }
 
     /**
      * 生成侧边栏
      *
-     * @param array $rightList 侧边列表数组
+     * @param array $originalData 侧边列表数组
      * @return string html代码
      */
-    private function makeRight ($rightList)
-    {
-        $return = '';
-        foreach ($rightList as $key => $file) {
-            $return .= '<blockquote class="layui-elem-quote layui-quote-nm right-item-title">' . $key . '</blockquote>
+    private function generateSidebar($originalData) {
+        $sidebarString = '';
+        foreach ($originalData as $key => $blockquote) {
+
+            $sidebarString .= '<blockquote class="layui-elem-quote layui-quote-nm right-item-title">' . $key . '</blockquote>
             <ul class="right-item">';
-            foreach ($file as $one) {
-                $return .= '<li><a href="#' . base64_encode($one['requestUrl']) . '"><cite>' . $one['methodName'] . '</cite><em>' . $one['requestUrl'] . '</em></a></li>';
+
+            foreach ($blockquote as $li) {
+
+                $sidebarString .= '<li><a href="#' . base64_encode($li['requestUrl']) . '">';
+                $sidebarString .= "<cite>{$li['methodName']}</cite>";
+                $sidebarString .= "<em>{$li['requestUrl']}</em></a></li>";
+
             }
-            $return .= '</ul>';
+            $sidebarString .= '</ul>';
         }
 
-        return $return;
+        return $sidebarString;
     }
 
     /**
      * @return string
      */
-    protected function getDocumentPath (): string
-    {
+    protected function getDocumentPath(): string {
         return $this->documentPath;
     }
 
     /**
      * @param string $documentPath
      */
-    protected function setDocumentPath ($documentPath): void
-    {
+    protected function setDocumentPath($documentPath): void {
         $this->documentPath = $documentPath;
     }
 
     /**
      * @return string
      */
-    protected function getSavePath (): string
-    {
+    protected function getSavePath(): string {
         return $this->savePath;
     }
 
     /**
      * @param string $savePath
      */
-    protected function setSavePath ($savePath): void
-    {
+    protected function setSavePath($savePath): void {
         $this->savePath = $savePath;
     }
 
     /**
      * @return string
      */
-    protected function getName (): string
-    {
+    protected function getName(): string {
         return $this->name;
     }
 
@@ -499,24 +526,21 @@ class ApiDoc
      *
      * @param string $name
      */
-    public function setName ($name): void
-    {
+    public function setName($name): void {
         $this->name = $name;
     }
 
     /**
      * @return string
      */
-    protected function getTemplateName (): string
-    {
+    protected function getTemplateName(): string {
         return $this->templateName;
     }
 
     /**
      * @param string $templateName
      */
-    public function setTemplateName (string $templateName): void
-    {
+    public function setTemplateName(string $templateName): void {
         $this->templateName = $templateName;
     }
 
@@ -525,8 +549,7 @@ class ApiDoc
      *
      * @return string
      */
-    protected function getSaveFilePathAndName ()
-    {
+    protected function getSaveFilePathAndName() {
         return $this->getSavePath() . $this->getName() . self::TEMPLATE_FILE_EXTENSION;
     }
 }
